f8ab27a4997700c7cda01835f4e86bf71d8b7cdc
djdrewrn-hub <djdrewrn@gmail.com>
2026-02-15 03:16:28 +0000

    code

--- homework8_david_drew.py
+++ homework8_david_drew.py
@@ -1,1 +1,9 @@
+def sandwich_ingredients(*ingredients):
+    print("The ingredients for the sandwich are:")
+    for ingredient in ingredients:
+        print("- " + ingredient)
+
+sandwich_ingredients("white bread", "bacon", "lettuce", "tomato", "mayo")
+sandwich_ingredients("rye bread", "corned beef", "sauerkraut", "thousand island dressing", "swiss cheese")
+
 
